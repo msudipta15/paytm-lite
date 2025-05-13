34b89bf0e35e87b63da3108739ffbe3907c34e0f
Sudipta Mondal <techtv70@gmail.com>
2025-05-13 20:21:46 +0530

    build backend for transaction history , working on fe for the same

--- frontend/src/components/transaction.tsx
+++ frontend/src/components/transaction.tsx
@@ -2,7 +2,7 @@ export function Transaction() {
   return (
     <div className="w-full h-fit p-3 text-xl bg-white flex justify-between items-center">
       <div className="ml-10">
-        <div>Name</div>
+        <div>Fname Lname</div>
         <div>time</div>
       </div>
       <div className="mr-10">Credit</div>
